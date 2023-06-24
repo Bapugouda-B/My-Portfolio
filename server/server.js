fc9323c7355db374ed64862849a96b61b00dd20d
Bapugouda <rajjdanush@gmail.com>
2023-06-24 17:59:00 +0530

    Increase the timeout value for mongodb server to respond

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ console.log("MONGODB URI:", process.env.MONGODB);
 mongoose.connect(process.env.MONGODB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
 })
 .then(() => console.log("MongoDB connected..."))
 .catch((err) => console.log(err));
